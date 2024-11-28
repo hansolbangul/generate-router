e777e5a1b4f2fba87f9fb9686aa91ed7f7140145
hansol-apr <ruaenddl981028@naver.com>
2024-11-28 11:27:19 +0900

    [Fix] bin set error fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,33 @@
 import typescript from 'rollup-plugin-typescript2';
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
-      file: './dist/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: './dist/index.esm.js',
-      format: 'esm',
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript()],
-};
+export default [
+  // Main Library Build
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        file: './dist/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: './dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript()],
+  },
+  {
+    input: './src/cli.ts',
+    output: [
+      {
+        file: './dist/cli.js',
+        format: 'cjs',
+        sourcemap: true,
+        banner: '#!/usr/bin/env node',
+      },
+    ],
+    plugins: [typescript()],
+  },
+];
